c4fdf23503521fe1765695b7689c5f8ac5efe345
Work Account <handbaccount@ghadahs-imac.local>
2019-02-21 20:49:28 +0000

    Initial Commit

---
+++ learn_design_ios/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  learn_design_ios
+//
+//  Created by Hobbies&Business on 21/02/2019.
+//  Copyright © 2019 Hobbies&Business. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ learn_design_ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  learn_design_ios
+//
+//  Created by Hobbies&Business on 21/02/2019.
+//  Copyright © 2019 Hobbies&Business. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
